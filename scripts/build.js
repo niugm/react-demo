b5899dda39765a709c9ee4a76d260f954062b592
niugm <niuguomeng@gmail.com>
2018-11-21 17:56:01 +0800

    配置更新

--- scripts/build.js
+++ scripts/build.js
@@ -131,7 +131,7 @@ function build(previousFileSizes) {
         }
         messages = formatWebpackMessages({
           errors: [err.message],
-          warnings: []
+          warnings: [],
         });
       } else {
         messages = formatWebpackMessages(
@@ -164,7 +164,7 @@ function build(previousFileSizes) {
       const resolveArgs = {
         stats,
         previousFileSizes,
-        warnings: messages.warnings
+        warnings: messages.warnings,
       };
       if (writeStatsJson) {
         return bfj
@@ -181,6 +181,6 @@ function build(previousFileSizes) {
 function copyPublicFolder() {
   fs.copySync(paths.appPublic, paths.appBuild, {
     dereference: true,
-    filter: file => file !== paths.appHtml
+    filter: file => file !== paths.appHtml,
   });
 }
