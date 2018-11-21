6aaa3b6fc99ea6d5121ea36c91c3c7b325fc33ef
niugm <niuguomeng@gmail.com>
2018-11-21 15:49:51 +0800

    prettier 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,28 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
   },
-  "extends": "react-app",
-  "parserOptions": {
-    "ecmaVersion": {
-      // http://es6.ruanyifeng.com/#docs/object#对象的扩展运算符  
-      "experimentalObjectRestSpread": true,
-      "jsx": true
+  extends: 'react-app',
+  parserOptions: {
+    ecmaVersion: {
+      // http://es6.ruanyifeng.com/#docs/object#对象的扩展运算符
+      experimentalObjectRestSpread: true,
+      jsx: true,
     },
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "modules": true
-    }
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
   },
-  "plugins": [
-    "react",
-    "prettier"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-      4
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
-    "no-unused-vars": 0,
-    "prettier/prettier": "error"
-  }
+  plugins: ['react', 'prettier'],
+  rules: {
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': 0,
+    'prettier/prettier': 'error',
+  },
 };
